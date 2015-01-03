772223f84220b7a4fdecc5fa928bda3df1bb199a
vdemedes <vdemedes@gmail.com>
2015-01-03 15:58:44 +0200

    embracing ES6

--- test/mailman.test.js
+++ test/mailman.test.js
@@ -14,59 +14,71 @@ require('mocha-generators')();
 describe ('Mailman', function () {
 	var user = 'test_user';
 	var password = 'test_password';
+	var sender = 'sender@test.com';
+	var receiver = 'receiver@test.com';
 	
 	before (function () {
 		Mailman.options.views.path = __dirname + '/fixtures/views';
 	});
 	
-	it ('should configure known transport', function () {
-		// configuring gmail as an example
-		Mailman.configure('gmail', {
-			user: user,
-			password: password
-		});
-		
-		var transport = Mailman.transport.transporter.options;
-		transport.service.should.equal('gmail');
-		transport.auth.user.should.equal(user);
-		transport.auth.pass.should.equal(password);
-		transport.host.should.equal('smtp.gmail.com');
-		transport.port.should.equal(465);
-		transport.secure.should.equal(true);
-	});
+  it ('should configure known transport', function () {
+   // configuring gmail as an example
+   Mailman.configure('gmail', { user, password });
+   
+   var transport = Mailman.transport.transporter.options;
+   transport.service.should.equal('gmail');
+   transport.auth.user.should.equal(user);
+   transport.auth.pass.should.equal(password);
+   transport.host.should.equal('smtp.gmail.com');
+   transport.port.should.equal(465);
+   transport.secure.should.equal(true);
+  });
+  
+  it ('should configure smtp transport', function () {
+   Mailman.configure({
+      host: 'smtp.gmail.com',
+      port: 587,
+      auth: { user, pass: password }
+   });
+   
+   var transport = Mailman.transport.transporter.options;
+   transport.auth.user.should.equal(user);
+   transport.auth.pass.should.equal(password);
+   transport.host.should.equal('smtp.gmail.com');
+   transport.port.should.equal(587);
+  });
 	
-	it ('should configure smtp transport', function () {
-		Mailman.configure({
-		   host: 'smtp.gmail.com',
-		   port: 465,
-		   secure: true,
-		   auth: {
-		       user: user,
-		       pass: password
-		   } 
-		});
-		
-		var transport = Mailman.transport.transporter.options;
-		transport.auth.user.should.equal(user);
-		transport.auth.pass.should.equal(password);
-		transport.host.should.equal('smtp.gmail.com');
-		transport.port.should.equal(465);
-		transport.secure.should.equal(true);
-	});
-	
-	it ('should define a mailer and mail', function *() {
-		var UserMailer = Mailman.Mailer.extend({
-			name: 'user',
-			from: 'test@test.com',
+	it ('should define a mailer and mail', function () {
+		class UserMailer extends Mailman.Mailer {
+		  get name () { return 'user'; }
+		  get from () { return sender; }
 			
-			welcome: function () {
+			welcome () {
 				this.full_name = 'John Doe';
 			}
-		});
+		}
 		
-		var mail = new UserMailer({ to: 'receiver@test.com' }).welcome();
+		var mail = new UserMailer({ to: receiver }).welcome();
 		mail.locals.full_name.should.equal('John Doe');
 		mail.options.view.should.equal('user/welcome.ejs');
-		mail.options.from.should.equal('test@test.com');
+		mail.options.from.should.equal(sender);
+	});
+	
+	describe ('Backwards compatibility', function () {
+	  it ('should define a mailer and mail with ES5-ish API', function () {
+      let UserMailer = Mailman.Mailer.extend({
+        name: 'user',
+        from: sender,
+        
+        welcome: function () {
+          this.full_name = 'John Doe';
+        }
+      });
+
+  		var mail = new UserMailer({ to: receiver }).welcome();
+  		mail.locals.full_name.should.equal('John Doe');
+  		mail.options.view.should.equal('user/welcome.ejs');
+  		mail.options.from.should.equal(sender);
+	  });
 	});
 });
--- test/test.js
+++ test/test.js
@@ -2,4 +2,4 @@ require('6to5/register')({
   blacklist: ['generators']
 });
 
-module.exports = require('./lib/mailman');
+require('./mailman.test');
