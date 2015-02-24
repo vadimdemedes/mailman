f156129c4b6c8ca9e540586d8b055fc716e2c8bb
Nicholas Young <nicholas@nicholaswyoung.com>
2015-02-24 10:37:19 -0600

    Revert "Add coreAliasing: Object.assign(). Closes #2."
    This reverts commit cfa3d35364b948e584218c426890c158357ebfed.

--- build/mailman.js
+++ build/mailman.js
@@ -16,11 +16,6 @@ var _slicedToArray = function (arr, i) {
   }
 };
 
-var _prototypeProperties = function (child, staticProps, instanceProps) {
-  if (staticProps) Object.defineProperties(child, staticProps);
-  if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
-};
-
 /**
  * Dependencies
  */
@@ -40,121 +35,98 @@ var walk = util.walk;
  * Mailman
  */
 
-var Mailman = (function () {
-  function Mailman() {}
+var Mailman = function Mailman() {};
 
-  _prototypeProperties(Mailman, {
-    configure: {
-      value: function configure(service, auth) {
-        var transport = undefined;
+Mailman.configure = function (service, auth) {
+  var transport = undefined;
 
-        // if service is a string, assume that it's
-        // one of the nodemailer-wellknown
-        // supported services
-        // see https://github.com/andris9/nodemailer-wellknown#supported-services
-        if ("string" === typeof service) {
-          transport = nodemailer.createTransport({
-            service: service,
-            auth: {
-              user: auth.user,
-              pass: auth.password
-            }
-          });
-        }
+  // if service is a string, assume that it's
+  // one of the nodemailer-wellknown
+  // supported services
+  // see https://github.com/andris9/nodemailer-wellknown#supported-services
+  if ("string" === typeof service) {
+    transport = nodemailer.createTransport({
+      service: service,
+      auth: {
+        user: auth.user,
+        pass: auth.password
+      }
+    });
+  }
 
-        if ("object" === typeof service) {
-          if ("undefined" === typeof service.sendMail) {
-            // if no sendMail property, assume
-            // that it's just normal object
+  if ("object" === typeof service) {
+    if ("undefined" === typeof service.sendMail) {
+      // if no sendMail property, assume
+      // that it's just normal object
 
-            transport = nodemailer.createTransport(service);
-          } else {
-            // else, it must be initialized
-            // 3rd-party transport
-            transport = service;
-          }
-        }
-
-        // transform sendMail into co-compatible fn
-        transport.sendMail = thunkify(transport.sendMail);
-
-        // set this transport as a default
-        return this.transport = transport;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    scanViews: {
-      value: function scanViews() {
-        var path = this.options.views.path;
-
-        if (!path) {
-          throw new Error("Mailman.options.views.path is empty");
-        }
-
-        var files = walk(path);
-        var views = {};
-
-        files.forEach(function (file) {
-          // /views/mailer_name/view_name.ejs => mailer_name
-          var _file$replace$split = file.replace(path + "/", "").split("/");
-
-          var _file$replace$split2 = _slicedToArray(_file$replace$split, 1);
-
-          var mailerName = _file$replace$split2[0];
-
-
-          // /views/mailer_name/view_name.ejs => view_name
-          var viewName = basename(file).replace(/\.[a-z]{3,4}$/g, "");
-
-          // under_score to camelCase
-          viewName = viewName.replace(/\_[a-z0-9]/gi, function ($1) {
-            return $1.slice(1).toUpperCase();
-          });
-
-          if (!views[mailerName]) views[mailerName] = {};
-
-          // register view and store it's relative path
-          views[mailerName][viewName] = mailerName + "/" + basename(file);
-        });
-
-        this.views = views;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    renderView: {
-      value: function renderView(path, locals) {
-        if (!this.render) {
-          var options = this.options.views;
-
-          this.render = views(options.path, {
-            cache: options.cache,
-            "default": options["default"],
-            map: options.map
-          });
-        }
-
-        return this.render(path, locals);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    send: {
-      value: function send(options) {
-        return this.transport.sendMail(options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
+      transport = nodemailer.createTransport(service);
+    } else {
+      // else, it must be initialized
+      // 3rd-party transport
+      transport = service;
     }
+  }
+
+  // transform sendMail into co-compatible fn
+  transport.sendMail = thunkify(transport.sendMail);
+
+  // set this transport as a default
+  return this.transport = transport;
+};
+
+Mailman.scanViews = function () {
+  var path = this.options.views.path;
+
+  if (!path) {
+    throw new Error("Mailman.options.views.path is empty");
+  }
+
+  var files = walk(path);
+  var _views = {};
+
+  files.forEach(function (file) {
+    // /views/mailer_name/view_name.ejs => mailer_name
+    var _ref = file.replace(path + "/", "").split("/");
+
+    var _ref2 = _slicedToArray(_ref, 1);
+
+    var mailerName = _ref2[0];
+
+
+    // /views/mailer_name/view_name.ejs => view_name
+    var viewName = basename(file).replace(/\.[a-z]{3,4}$/g, "");
+
+    // under_score to camelCase
+    viewName = viewName.replace(/\_[a-z0-9]/gi, function ($1) {
+      return $1.slice(1).toUpperCase();
+    });
+
+    if (!_views[mailerName]) _views[mailerName] = {};
+
+    // register view and store it's relative path
+    _views[mailerName][viewName] = mailerName + "/" + basename(file);
   });
 
-  return Mailman;
-})();
+  this.views = _views;
+};
+
+Mailman.renderView = function (path, locals) {
+  if (!this.render) {
+    var _options = this.options.views;
+
+    this.render = views(_options.path, {
+      cache: _options.cache,
+      "default": _options["default"],
+      map: _options.map
+    });
+  }
+
+  return this.render(path, locals);
+};
+
+Mailman.send = function (options) {
+  return this.transport.sendMail(options);
+};
 
 Mailman.options = {
   views: {
@@ -171,33 +143,22 @@ Mailman.options = {
  */
 
 
-var Mail = (function () {
-  function Mail() {
-    var options = arguments[0] === undefined ? {} : arguments[0];
-    var locals = arguments[1] === undefined ? {} : arguments[1];
-    this.options = options;
-    this.locals = locals;
-  }
+var Mail = function Mail(options, locals) {
+  if (options === undefined) options = {};
+  if (locals === undefined) locals = {};
+  this.options = options;
+  this.locals = locals;
+};
 
-  _prototypeProperties(Mail, null, {
-    deliver: {
-      value: function* deliver() {
-        // clone this.options
-        var options = Object.assign({}, this.options);
+Mail.prototype.deliver = function* () {
+  // clone this.options
+  var _options2 = Object.assign({}, this.options);
 
-        // render email body
-        options.html = yield Mailman.renderView(options.view, this.locals);
+  // render email body
+  _options2.html = yield Mailman.renderView(_options2.view, this.locals);
 
-        yield Mailman.send(options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    }
-  });
-
-  return Mail;
-})();
+  yield Mailman.send(_options2);
+};
 
 
 
@@ -212,75 +173,60 @@ var supportedOptions = ["from", "to", "cc", "bcc", "replyTo", "inReplyTo", "refe
 // mailman properties to ignore
 var ignoredKeys = ["name", "transport"];
 
-var Mailer = (function () {
-  function Mailer() {
-    var options = arguments[0] === undefined ? {} : arguments[0];
-    Object.assign(this, options);
+var Mailer = function Mailer(options) {
+  if (options === undefined) options = {};
+  Object.assign(this, options);
 
-    if (!this.transport) this.transport = Mailman.transport;
+  if (!this.transport) this.transport = Mailman.transport;
 
-    if (!Mailman.views) Mailman.scanViews();
+  if (!Mailman.views) Mailman.scanViews();
 
-    // iterate over this object
-    for (var key in this) {
-      // if this method name registered
-      // as a view, wrap it into Mail factory
-      if (Mailman.views[this.name] && Mailman.views[this.name][key]) {
-        this[key] = factory(this, key);
-      }
+  // iterate over this object
+  for (var key in this) {
+    // if this method name registered
+    // as a view, wrap it into Mail factory
+    if (Mailman.views[this.name] && Mailman.views[this.name][key]) {
+      this[key] = factory(this, key);
+    }
+  }
+};
+
+Mailer.prototype.options = function () {
+  var _this = this;
+  var _options3 = {};
+
+  // get only nodemailer supported properties
+  supportedOptions.forEach(function (key) {
+    return _options3[key] = _this[key];
+  });
+
+  return _options3;
+};
+
+Mailer.prototype.locals = function () {
+  var _locals = {};
+
+  for (var key in this) {
+    var value = this[key];
+
+    // ignored keys, mailer name and transport
+    var isIgnored = ignoredKeys.includes(key);
+
+    // nodemailer options
+    var isOption = supportedOptions.includes(key);
+
+    // also, don't append functions
+    var isFunction = "function" === typeof value;
+
+    // if none of those is true
+    // apend new template variable
+    if (!isIgnored && !isOption && !isFunction) {
+      _locals[key] = value;
     }
   }
 
-  _prototypeProperties(Mailer, null, {
-    options: {
-      value: function options() {
-        var _this = this;
-        var options = {};
-
-        // get only nodemailer supported properties
-        supportedOptions.forEach(function (key) {
-          return options[key] = _this[key];
-        });
-
-        return options;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    locals: {
-      value: function locals() {
-        var locals = {};
-
-        for (var key in this) {
-          var value = this[key];
-
-          // ignored keys, mailer name and transport
-          var isIgnored = ignoredKeys.includes(key);
-
-          // nodemailer options
-          var isOption = supportedOptions.includes(key);
-
-          // also, don't append functions
-          var isFunction = "function" === typeof value;
-
-          // if none of those is true
-          // apend new template variable
-          if (!isIgnored && !isOption && !isFunction) {
-            locals[key] = value;
-          }
-        }
-
-        return locals;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    }
-  });
-
-  return Mailer;
-})();
+  return _locals;
+};
 
 Mailer.extend = Class.extend;
 
@@ -293,13 +239,13 @@ function factory(mailer, method) {
   return function () {
     fn.apply(mailer, arguments);
 
-    var options = mailer.options();
-    var locals = mailer.locals();
+    var _options4 = mailer.options();
+    var _locals2 = mailer.locals();
 
     // get path for a view
-    options.view = Mailman.views[this.name][method];
+    _options4.view = Mailman.views[this.name][method];
 
-    return new Mail(options, locals);
+    return new Mail(_options4, _locals2);
   };
 }
 
